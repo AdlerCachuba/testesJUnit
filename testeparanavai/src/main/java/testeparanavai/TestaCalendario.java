9e1e9db27595fe2672c8f54600f71e9274ce54bf
fulano de tal <bistecaEletrica@gmail.com>
2022-05-23 22:21:18 -0300

    Projeto Inicial dos testes

---
+++ testeparanavai/src/main/java/testeparanavai/TestaCalendario.java
@@ -0,0 +1,22 @@
+package testeparanavai;
+
+import paranavai.calendario.Calendario;
+
+public class TestaCalendario {
+public static void main(String[] args) {
+	Calendario.mostrarCalendario("2022"); //(v1)
+	//Calendario.mostrarCalendario("12","12"); //(v2)
+
+//	Calendario.mostrarCalendario("e"); // i3..ano inválido..
+//	Calendario.mostrarCalendario("0"); //(i4)
+//	Calendario.mostrarCalendario("10000"); // (i5)
+//	Calendario.mostrarCalendario("2022"); // V(6)
+//	Calendario.mostrarCalendario("5,5","6,7"); //V(8)
+//	Calendario.mostrarCalendario("5,5","0"); //V(9)
+//	Calendario.mostrarCalendario("5,5","10000"); //V(10)
+//	Calendario.mostrarCalendario("5,5","2022"); //V(11)
+//	Calendario.mostrarCalendario("8","7,6"); //V(11)
+//	
+
+}
+}
